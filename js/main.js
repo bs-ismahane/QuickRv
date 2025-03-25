368156d00a574e24c17c75c40c90997e8a9032b3
ismahane bensaada <ismahane20050708@gmail.com>
2025-03-25 03:09:47 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,20 +5,6 @@ window.addEventListener("load",function(){
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
 })
-const navBtn = document.querySelector(".nav-btn");
-const navBar = document.querySelector(".navbar");
-
-navBtn.addEventListener("click", function () {
-    if (navBtn.classList.contains("active")) {
-        navBtn.classList.remove("active");
-        navBar.classList.remove("active"); 
-        navBtn.innerHTML = `<i class="fa-solid fa-bars"></i>`; 
-    } else {
-        navBtn.classList.add("active");
-        navBar.classList.add("active");
-        navBtn.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
-    }
-});
 // hero
 const hero = document.querySelector(".hero");
 const bg = ['./images/image1.jpg', './images/image2.jpg', './images/image3.jpg'];
